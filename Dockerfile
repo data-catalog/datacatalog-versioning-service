62c59b4a43bd343ed50a3eaf6a7abd5ded5fda20
Szakács Róbert <robertszakacs29@gmail.coom>
2020-12-11 17:35:17 +0200

    Spring setup

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:6.6.1-jdk11-hotspot AS stage1
+COPY --chown=gradle:gradle . /usr/data-catalog-versioning-service
+WORKDIR /usr/datacatalog
+RUN gradle bootJar
+
+FROM openjdk:11-slim
+COPY --from=stage1 /usr/data-catalog-versioning-service /usr/data-catalog-versioning-service
+CMD java -jar /usr/data-catalog-versioning-service/build/libs/versioning-api-1.0.0-SNAPSHOT.jar
+
+EXPOSE 3000
---
+++ src/main/java/edu/bbte/projectbluebook/datacatalog/datacatalogversioningservice/DatacatalogVersioningServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.bbte.projectbluebook.datacatalog.datacatalogversioningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacatalogVersioningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacatalogVersioningServiceApplication.class, args);
+	}
+
+}
