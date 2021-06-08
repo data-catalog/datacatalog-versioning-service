8bb0048e8cdaaa25142a5cec1d9cc5cbdccf7ed0
Tumó Szilárd <tumoszilard@gmail.com>
2021-06-08 13:14:58 +0300

    Fixed checkstyle warning.

--- src/main/java/edu/bbte/projectbluebook/datacatalog/versioning/config/GlobalCorsConfiguration.java
+++ src/main/java/edu/bbte/projectbluebook/datacatalog/versioning/config/GlobalCorsConfiguration.java
@@ -3,7 +3,6 @@ package edu.bbte.projectbluebook.datacatalog.versioning.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
