843af4bf39acb32289f44da6b217978b2457af79
Tumó Szilárd <tumoszilard@gmail.com>
2021-06-08 13:12:56 +0300

    Fixed date serialization.

--- src/main/java/edu/bbte/projectbluebook/datacatalog/versioning/config/GlobalCorsConfiguration.java
+++ src/main/java/edu/bbte/projectbluebook/datacatalog/versioning/config/GlobalCorsConfiguration.java
@@ -1,17 +1,26 @@
 package edu.bbte.projectbluebook.datacatalog.versioning.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
-@EnableWebFlux
-public class GlobalCorsConfiguration implements WebFluxConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS");
+public class GlobalCorsConfiguration {
+
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new CorsWebFluxConfigurer();
+    }
+
+    static class CorsWebFluxConfigurer extends WebFluxConfigurerComposite {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS");
+        }
     }
 }
